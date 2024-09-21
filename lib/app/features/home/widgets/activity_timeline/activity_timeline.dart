65a404993c5c27fa02ab403259643ed6d559aea5
Deepacks <vladimirsette.vc@gmail.com>
2024-09-21 19:16:53 +0200

    feat: group activities by time

--- lib/app/features/home/widgets/activity_timeline/activity_timeline.dart
+++ lib/app/features/home/widgets/activity_timeline/activity_timeline.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:track_me/app/features/home/helpers/group_activities.dart';
 import 'package:track_me/app/features/home/providers/activity_provider.dart';
 import 'package:track_me/app/features/home/providers/activity_type_provider.dart';
 import 'package:track_me/app/features/home/widgets/activity_timeline/activity_timeline_tile.dart';
@@ -10,24 +11,38 @@ class ActivityTimeline extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final activityState = ref.watch(activityNotifierProvider);
+    final activities = ref.watch(
+      activityNotifierProvider.select((state) => state.activities),
+    );
     final activityTypes = ref.watch(
       activityTypeNotifierProvider.select((state) => state.activityTypes),
     );
 
+    final groupedActivitiesEntries =
+        groupActivities(activities).entries.toList();
+
     return Expanded(
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            for (final activity in activityState.activities) ...[
-              ActivityTimelineTile(
-                activity: activity,
-                activityType: activityTypes.firstWhere(
-                  (activityType) => activityType.id == activity.activityTypeId,
+            for (final group in groupedActivitiesEntries)
+              if (group.value.isNotEmpty) ...[
+                Text(group.key),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              const SizedBox(height: 10),
-            ],
+                for (final activity in group.value) ...[
+                  ActivityTimelineTile(
+                    activity: activity,
+                    activityType: activityTypes.firstWhere(
+                      (activityType) =>
+                          activityType.id == activity.activityTypeId,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ],
           ],
         ),
       ),
